fdcd2d4810d857d23b957bde3f23a6e1be038915
Fadhil <agungfadhil23@gmail.com>
2019-11-18 01:09:32 +0700

    setting router

--- src/pages/City.js
+++ src/pages/City.js
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { API } from "../config/api";
+import CategoryList from "../components/CategoryList";
+import SearchKeyword from "../components/SearchKeyword";
+import SearchCriteria from "../components/SearchCriteria";
+import RestaurantCard from "../components/RestaurantCard";
 
 class City extends Component {
   constructor() {
     super();
     this.state = {
-      city: null
+      city: null,
+      categories: null,
+      categorySelected: null,
+      keyword: "",
+      criteria: [],
+      restaurants: []
     };
   }
+
+  searchHandler = () => {
+    this.setState({ restaurants: null });
+    let url = `${API.zomato.baseUrl}/search`;
+    let params = {};
+
+    for (let cri of this.state.criteria) {
+      switch (cri.criteriaName) {
+        case "City":
+          params.entity_id = cri.data.id;
+          params.entity_type = "city";
+          break;
+        case "Category":
+          params.q = cri.data.id;
+          break;
+        case "Keyword":
+          params.q = cri.data.name;
+          break;
+
+        default:
+          break;
+      }
+    }
+    axios
+      .get(url, {
+        headers: {
+          "user-key": API.zomato.api_key
+        },
+        params
+      })
+      .then(({ data }) => {
+        this.setState({ restaurants: data.restaurants });
+      })
+      .catch(err => console.log(err));
+  };
+  transformCategoriesData = categories => {
+    let categoriesTransformed = categories.map(category => {
+      return category.categories;
+    });
+    return categoriesTransformed;
+  };
+
   getCityData = city_id => {
     let url = `${API.zomato.baseUrl}/cities`;
     axios
@@ -22,14 +73,86 @@ class City extends Component {
       })
       .then(({ data }) => {
         let city = data.location_suggestions[0];
-        this.setState({ city });
+        let newCriteria = {
+          criteriaName: "City",
+          data: city
+        };
+        let criteria = [...this.state.criteria];
+        criteria.push(newCriteria);
+
+        this.setState({ city, criteria });
       })
       .catch(err => console.log(err));
   };
   componentDidMount() {
     let { city_id } = this.props.match.params;
     this.getCityData(city_id);
+    this.getCategoriesData();
   }
+  categoryClickHandler = category => {
+    let criteria = [...this.state.criteria];
+    criteria = criteria.filter(cri => cri.criteriaName !== "Category");
+    let newCriteria = {
+      criteriaName: "Category",
+      data: category
+    };
+    criteria.push(newCriteria);
+    this.setState({ categorySelected: category, criteria });
+  };
+  changeKeywordHandler = event => {
+    this.setState({ keyword: event.target.value });
+  };
+  addToCriteriaHandler = () => {
+    let criteria = [...this.state.criteria];
+    criteria = criteria.filter(cri => cri.criteriaName !== "Keyword");
+    let newCriteria = {
+      criteriaName: "Keyword",
+      data: {
+        name: this.state.keyword
+      }
+    };
+    criteria.push(newCriteria);
+    this.setState({ keyword: "", criteria });
+  };
+  removeCriteriaHandler = index => {
+    let criteria = [...this.state.criteria];
+    criteria.splice(index, 1);
+    this.setState({ criteria });
+  };
+  getCategoriesData = () => {
+    let url = `${API.zomato.baseUrl}/categories`;
+    axios
+      .get(url, {
+        headers: {
+          "user-key": API.zomato.api_key
+        }
+      })
+      .then(({ data }) => {
+        let categories = this.transformCategoriesData(data.categories);
+        this.setState({ categories });
+      })
+      .catch(err => console.log(err));
+  };
+  renderRestaurantList = () => {
+    if (this.state.restaurants === null) {
+      return (
+        <div className="col">
+          <p>Loading...</p>
+        </div>
+      );
+    }
+    if (this.state.restaurants.length > 0) {
+      return this.state.restaurants.map(({ restaurant }) => (
+        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+      ));
+    } else {
+      return (
+        <div className="col">
+          <p>No Data Available. Please select criteria and click search</p>
+        </div>
+      );
+    }
+  };
   render() {
     return (
       <div
@@ -46,6 +169,37 @@ class City extends Component {
             </div>
           </div>
         )}
+
+        <div className="row">
+          <div className="col-3">
+            <h5>Categories</h5>
+            <CategoryList
+              categories={this.state.categories}
+              categorySelected={this.state.categorySelected}
+              categoryClickHandler={category =>
+                this.categoryClickHandler(category)
+              }
+            />
+          </div>
+          <div className="col">
+            <SearchKeyword
+              onClickAddToCriteria={this.addToCriteriaHandler}
+              keyword={this.state.keyword}
+              changeKeywordHandler={this.changeKeywordHandler}
+            />
+            <SearchCriteria
+              criteria={this.state.criteria}
+              removeCriteriaHandler={index => this.removeCriteriaHandler(index)}
+              onClickSearch={this.searchHandler}
+            />
+            <div className="row">
+              <div className="col" style={{ marginBottom: 10 }}>
+                <h4 className="text-success">Restaurant List</h4>
+              </div>
+            </div>
+            <div className="row">{this.renderRestaurantList()}</div>
+          </div>
+        </div>
       </div>
     );
   }
