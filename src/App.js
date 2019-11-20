7f26ca07296d1aff51158b77b8ba759d8edefb75
Fadhil <agungfadhil23@gmail.com>
2019-11-21 01:31:55 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from "./pages/Home";
 import City from "./pages/City";
+import RestaurantDetail from "./pages/RestaurantDetail"
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -11,8 +12,9 @@ class App extends Component {
     return (
       <Router>
         <Navbar />
-        <Route path="/" exact component={Home} />
+        <Route path="/" component={Home} exact />
         <Route path="/city/:city_id" component={City} />
+        <Route path="/restaurant/:restaurant_id" component={RestaurantDetail} />
         <Footer />
       </Router>
     );
