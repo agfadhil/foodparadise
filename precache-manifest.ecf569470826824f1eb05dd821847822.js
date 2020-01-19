2104c3fe50b61b26d869a76d1a33642f2d8e6539
Fadhil <agungfadhil23@gmail.com>
2020-01-19 20:27:21 +0700

    pure build folder

---
+++ precache-manifest.ecf569470826824f1eb05dd821847822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed055baed7a26b7450da7e4d5531b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df903046210ad48811a6",
+    "url": "/static/css/main.c5777c75.chunk.css"
+  },
+  {
+    "revision": "3c2b1f1981c797b81b6a",
+    "url": "/static/js/2.ba5ccfa2.chunk.js"
+  },
+  {
+    "revision": "df903046210ad48811a6",
+    "url": "/static/js/main.a8f538bd.chunk.js"
+  },
+  {
+    "revision": "80d7a922cad4f5d24f40",
+    "url": "/static/js/runtime-main.dc52788b.js"
+  },
+  {
+    "revision": "7998eb99cfc6f64c59847a8db7ff27d9",
+    "url": "/static/media/aneka-makanan.7998eb99.jpg"
+  }
+]);
